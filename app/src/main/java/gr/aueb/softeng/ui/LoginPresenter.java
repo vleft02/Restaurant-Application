58a82aa02b329e0110e773dd542502f76cb2b067
vleft <vleftakis@gmail.com>
2023-05-18 18:51:31 +0300

    small changes in login activity, viewmodel layout and presenter

--- app/src/main/java/gr/aueb/softeng/ui/LoginPresenter.java
+++ app/src/main/java/gr/aueb/softeng/ui/LoginPresenter.java
@@ -1,4 +1,18 @@
 package gr.aueb.softeng.ui;
 
 public class LoginPresenter {
+    //UserDAO
+    public void authenticate(String username,String password)
+    {
+        if (username==null || password == null)
+        {
+            //Error
+        }
+        else
+        {
+            //UserDAO.findUser();
+        }
+
+
+    }
 }
