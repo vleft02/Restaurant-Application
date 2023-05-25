507cce4499add0a719209023c7a82a6d05bb466e
vleft <vleftakis@gmail.com>
2023-05-25 15:53:07 +0300

    customer homepage activity, layout and presenter declared

---
+++ app/src/main/java/gr/aueb/softeng/view/Customer/CustomerHomepageActivity.java
@@ -0,0 +1,24 @@
+package gr.aueb.softeng.view.Customer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import gr.aueb.softeng.team08.R;
+
+public class CustomerHomepageActivity extends AppCompatActivity implements CustomerHomepageView{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        chooseLayout();
+        setContentView(R.layout.activity_customer_hompepage);
+
+
+    }
+
+    @Override
+    public void chooseLayout() {
+
+    }
+}
---
+++ app/src/main/java/gr/aueb/softeng/view/Customer/CustomerHomepageView.java
@@ -0,0 +1,6 @@
+package gr.aueb.softeng.view.Customer;
+import gr.aueb.softeng.view.View;
+
+public interface CustomerHomepageView extends View{
+void chooseLayout();
+}
