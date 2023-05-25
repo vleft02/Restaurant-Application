31bb5ec79dae1bcdda85ac1ebcc73387f2bcccaa
vleft <vleftakis@gmail.com>
2023-05-25 15:53:07 +0300

    customer homepage activity, layout and presenter declared

---
+++ app/src/main/java/gr/aueb/softeng/view/Customer/CustomerHomepagePresenter.java
@@ -0,0 +1,9 @@
+package gr.aueb.softeng.view.Customer;
+
+public class CustomerHomepagePresenter {
+    CustomerHomepageView view;
+
+    public void setView(CustomerHomepageView view) {
+        this.view = view;
+    }
+}
