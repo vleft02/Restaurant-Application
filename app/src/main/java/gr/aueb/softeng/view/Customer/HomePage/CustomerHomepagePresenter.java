5114c958dba31dc3b2f9702809de1c44d3f7fadd
vleft <vleftakis@gmail.com>
2023-06-01 14:38:39 +0300

    presenter change

--- app/src/main/java/gr/aueb/softeng/view/Customer/HomePage/CustomerHomepagePresenter.java
+++ app/src/main/java/gr/aueb/softeng/view/Customer/HomePage/CustomerHomepagePresenter.java
@@ -1,44 +1,82 @@
 package gr.aueb.softeng.view.Customer.HomePage;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
 
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+import gr.aueb.softeng.dao.ChefDAO;
 import gr.aueb.softeng.dao.CustomerDAO;
 import gr.aueb.softeng.dao.OrderDAO;
+import gr.aueb.softeng.domain.Customer;
 import gr.aueb.softeng.domain.Order;
 
 public class CustomerHomepagePresenter {
     CustomerDAO customerDAO;
     OrderDAO orderDAO;
-
+    ChefDAO chefDAO;
     CustomerHomepageView view;
+    CurrentOrderPageFragment currentOrderPageFragment;
+    OrderHistoryPageFragment orderHistoryPageFragment;
+    Order currentOrder;
+    Customer customer;
 
-    public CustomerHomepagePresenter(CustomerDAO customerDAO, OrderDAO orderDAO)
+    public CustomerHomepagePresenter(CustomerDAO customerDAO, OrderDAO orderDAO,ChefDAO chefDAO)
     {
         this.customerDAO = customerDAO;
         this.orderDAO = orderDAO;
+        this.chefDAO = chefDAO;
     }
 
-    public Order getCurrentOrder(int customerId)
+    public void setCustomer(int id) {
+        this.customer = customerDAO.find(id);
+    }
+
+    public void setCurrentOrder()
     {
-        ArrayList<Order> orders =  orderDAO.findByCustomer(customerId);
+        ArrayList<Order> orders = (ArrayList<Order>) orderDAO.findByCustomer(customer);
+        currentOrder = null;
         for (Order order : orders)
         {
-            if (order.getOrderState() != Order.State.COMPLETED)
+            if (order.getOrderState() != Order.State.COMPLETED && order.getOrderState() != Order.State.CANCELLED )
             {
-                return order;
+                currentOrder = order;
             }
         }
-        return null;
     }
+    public Order getCurrentOrder()
+    {
+        return currentOrder;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
     public void setView(CustomerHomepageView view) {
         this.view = view;
     }
 
-    public String getCurrentOrderDetails(int customerId) {
+    public void setFragment(Fragment fragment){
+        if (fragment instanceof CurrentOrderPageFragment) {
+            this.currentOrderPageFragment = (CurrentOrderPageFragment) fragment;
+
+        }
+        else
+        {
+            this.orderHistoryPageFragment = (OrderHistoryPageFragment) fragment;
+        }
+    }
+
+    public String getCurrentOrderDetails() {
         String output = "";
-        Order order = getCurrentOrder(customerId);
-        output += order.getOrderState().toString()+"\n";
+        if(currentOrder != null)
+        {
+            output += currentOrder.getOrderState().toString()+"\n";
+            output += currentOrder.getOrderState().toString()+"\n";
+        }
+
         return output;
     }
 
@@ -46,9 +84,38 @@ public class CustomerHomepagePresenter {
         view.ShowConfirmationMessage();
     }
 
-    public void cancel(int customerId)
+    public void cancel()
     {
-        orderDAO.find(getCurrentOrder(customerId)).setStateCancelled();
+       if(currentOrder!=null){
+            currentOrder.setStateCancelled();
+            currentOrder = null;
+            chooseLayout();
+            //AFAIRESH APO CHEF
+       }
+    }
+
+    public void chooseLayout() {
+
+        if (currentOrder!=null)
+        {
+            currentOrderPageFragment.plusButton.setVisibility(View.GONE);
+            currentOrderPageFragment.noOrderText.setVisibility(View.GONE);
+            currentOrderPageFragment.orderDetails.setText(getCurrentOrderDetails());
+            currentOrderPageFragment.cancelButton.setVisibility(View.VISIBLE);
+            currentOrderPageFragment.orderDetailsLayout.setVisibility(View.VISIBLE);
+
+
+        }
+        else{
+            currentOrderPageFragment.plusButton.setVisibility(View.VISIBLE);
+            currentOrderPageFragment.noOrderText.setVisibility(View.VISIBLE);
+            currentOrderPageFragment.cancelButton.setVisibility(View.GONE);
+            currentOrderPageFragment.orderDetailsLayout.setVisibility(View.GONE);
+        }
+    }
+
+    public void onTopUp() {
+        view.redirectTopUp();
     }
 }
 
