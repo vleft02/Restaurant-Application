9235735432f77c13e3e9baaf597d58d73e68ee7a
vleft <vleftakis@gmail.com>
2023-06-05 22:10:18 +0300

    commented CustomerHomePageActivity and CustomerHomepagePresenter

--- app/src/main/java/gr/aueb/softeng/view/Customer/HomePage/CustomerHomepagePresenter.java
+++ app/src/main/java/gr/aueb/softeng/view/Customer/HomePage/CustomerHomepagePresenter.java
@@ -29,6 +29,15 @@ public class CustomerHomepagePresenter {
 
     ArrayList<Order> orderHistory;
 
+    /**
+     * Αρχικοποιεί τον Presenter έτσι ώστε
+     * να μπορούμε να πραγματόποιήσουμε τις
+     * απαραίτητες λειτουργίες του customer
+     * @param customerDAO αντικείμενο τυπου CustomerDAO
+     * @param orderDAO αντικείμενο τυπου OrderDAO
+     * @param chefDAO αντικείμενο τύπου ChefDAO
+     * @param restaurantDAO αντικείμενο τυπου RestaurantDAO
+     */
     public CustomerHomepagePresenter(CustomerDAO customerDAO, OrderDAO orderDAO,ChefDAO chefDAO,RestaurantDAO restaurantDAO)
     {
         this.customerDAO = customerDAO;
@@ -38,10 +47,33 @@ public class CustomerHomepagePresenter {
         orderHistory = new ArrayList<>();
     }
 
+    /**
+     * Σετάρουμε την μεταβλητη restaurant
+     * βρίσκοντας τον απο το RestaurantDAO
+     * μέσω του id που δίνεται
+     * @param restaurantId το id του εστιατορίου
+     */
+    public void setRestaurant(int restaurantId) {
+
+        restaurant = restaurantDAO.find(restaurantId);
+    }
+    /**
+     * Σετάρουμε την μεταβλητη customer
+     * βρίσκοντας τον απο το CustomerDAO
+     * μέσω του id
+     * @param id το id του πελάτη
+     */
     public void setCustomer(int id) {
         this.customer = customerDAO.find(id);
     }
 
+    /**
+     * Σετάρουμε την λίστα orderHistory
+     * βρίσκοντας απο το orderDAO
+     * ποιές παράγγελίες απο τον σεταρισμένο customer
+     * είναι ολοκληρωμένες ή ακυρωμένες και
+     * προστιθοντας τες στο orderHistory
+     */
     public void setOrderHistory()
     {
         orderHistory = new ArrayList<>();
@@ -57,6 +89,14 @@ public class CustomerHomepagePresenter {
             }
         }
     }
+
+    /**
+     * Σετάρουμε την μεταβλητη currentOrder
+     * με την παραγγελία του σεταρισμένου
+     * πελάτη που δεν είναι ακυρωμένη ή ολοκληρωμένη
+     * εαν αυτή υπάρχει.
+     * (Δεν είναι δυνατόν να υπάρχουν παραπάνω απο μια τέτοιες)
+     */
     public void setCurrentOrder()
     {
         ArrayList<Order> orders = (ArrayList<Order>) orderDAO.findByCustomer(customer);
@@ -74,14 +114,37 @@ public class CustomerHomepagePresenter {
         return currentOrder;
     }
 
+    /**
+     * Επιστρέφει τον customer
+     * @return Το αντικέμενο πελατη τυπου Customer
+     */
     public Customer getCustomer() {
         return customer;
     }
 
+
+    /**
+     * επιστρέφει την λιστα orderHistory
+     * @return ArrayList<Order> με τις ανανεργές παραγγελίες
+     */
+    public ArrayList<Order> getOrderHistory() {
+        return orderHistory;
+    }
+    /**
+     * Επιστρέφει τον customer
+     * @param view ενα instance του view
+     */
     public void setView(CustomerHomepageView view) {
         this.view = view;
     }
 
+    /**
+     * Σετάρουμε την μετάβλητη fragment
+     * ανάλογα με το tab στο οποίο βρισκόμαστε
+     * (current order tab ή order history tab)
+     * για να έχουμε πρόσβαση στα
+     * γραφικά στοιχεία των fragments
+     */
     public void setFragment(Fragment fragment){
         if (fragment instanceof CurrentOrderPageFragment) {
             this.currentOrderPageFragment = (CurrentOrderPageFragment) fragment;
@@ -93,6 +156,11 @@ public class CustomerHomepagePresenter {
         }
     }
 
+
+    /**
+     * Επιστρέφει τα στοιχεία μιας παραγγελίας
+     * @return ένα String με τα στοιχεία της παραγγελίας currentOrder
+     */
     public String getCurrentOrderDetails() {
         String output = "";
         if(currentOrder != null)
@@ -105,6 +173,13 @@ public class CustomerHomepagePresenter {
         return output;
     }
 
+    /**
+     * Ακύρωση της currentOrder
+     * που προκαλέι αλλαγή του layout
+     * και της λιστας orderHistory
+     * αλλα και αφαίρεση της απο την λίστα
+     * παραγγελιών προς προετοιμασία απο τον chef
+     */
     public void cancel()
     {
        if(currentOrder!=null){
@@ -116,6 +191,11 @@ public class CustomerHomepagePresenter {
        }
     }
 
+    /**
+     * Αλλάζουμε layout για το currentOrderPageFragment
+     * ανάλογα με το αν υπάρχει σεταρισμένη
+     * ενεργή παραγγελία ή οχι
+     */
     public void chooseLayout() {
 
         if (currentOrder!=null)
@@ -136,23 +216,34 @@ public class CustomerHomepagePresenter {
         }
     }
 
+    /**
+     * κλήση της redirectTopUp() στο
+     * activity μέσω του interface customerHomePageView
+     * για προώθηση στο top Up activity
+     */
     public void onTopUp() {
         view.redirectTopUp();
     }
 
-    public ArrayList<Order> getOrderHistory() {
-        return orderHistory;
-    }
 
-    public void setRestaurant(int restaurantId) {
 
-        restaurant = restaurantDAO.find(restaurantId);
-    }
-
+    /**
+     * επιστρέφει τον μέγιστο αριθμό
+     * τραπεζιών του εστιατορίου
+     * @return εναν ακεραιο που δείχνει το πλήθος τραπεζιών του εσταιτορίου
+     */
     public int getRestaurantCapacity() {
         return restaurant.getTotalTables();
     }
 
+    /**
+     * Επιστρέφει false εαν υπάρχει ενεργή
+     * παραγγελία στο σεταρισμένο εστιατόριο
+     * στο τραπέζι που έχει επιλεγεί
+     * και true αν οχι
+     * @param tableNumber ο αριθμος ενός τραπεζίου
+     * @return true ή false
+     */
     public boolean checkTableAvailability(int tableNumber) {
         ArrayList<Order> orders = restaurant.getOrders();
         for (Order order:orders)
