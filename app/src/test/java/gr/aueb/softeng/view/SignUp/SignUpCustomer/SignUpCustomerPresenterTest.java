bf3ed1c4194dd8d5d199b3eee6e45d5cf1e28e26
k_platias <platiaskostas318@gmail.com>
2023-06-06 23:08:08 +0300

    some tests done

---
+++ app/src/test/java/gr/aueb/softeng/view/SignUp/SignUpCustomer/SignUpCustomerPresenterTest.java
@@ -0,0 +1,30 @@
+package gr.aueb.softeng.view.SignUp.SignUpCustomer;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SignUpCustomerPresenterTest {
+
+/*    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setView() {
+    }
+
+    @Test
+    public void onCreateAccount() {
+    }
+
+    @Test
+    public void onBack() {
+    }*/
+}
